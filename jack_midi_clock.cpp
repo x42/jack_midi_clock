87115a8abee8ca5774897e375c6d4b920cab4ef8
Robin Gareus <robin@gareus.org>
2013-05-15 19:29:26 +0200

    proper GPL boilerplate

--- jack_midi_clock.cpp
+++ jack_midi_clock.cpp
@@ -1,8 +1,24 @@
+/* jack midi clock -- generate MIDI Clock from JACK-Transport
+ *
+ * Copyright (C) 2009 Gabriel M. Beddingfield <gabriel@teuton.org>
+ * Copyright (C) 2004 Ian Esten (jackd's midiseq.c)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
-// Code starting point was midiseq.c from the JACK sources.
-
-
-
+#include <stdio.h>
 #include <jack/jack.h>
 #include <jack/midiport.h>
 #include <jack/transport.h>
